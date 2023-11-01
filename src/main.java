ad38cfafb33ebdae13f687ce1a8c57c99164b6f8
Denishttps <103338275+Denishttps@users.noreply.github.com>
2023-11-01 11:14:02 +0100

    Update main.java

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public String task1(Scanner scanner) {
+    public static String task1(Scanner scanner) {
         System.out.print("Geben Sie Ihren Benutzernamen ein: ");
         String username = scanner.nextLine();
 
@@ -14,10 +14,10 @@ public class Main {
         }
         return "Anmeldung fehlgeschlagen!";
     }
+    
 
-
-    public void task2(Scanner scanner) {
-        System.out.print("Willkommen beim Taschenrechner!");
+    public static void task2(Scanner scanner) {
+        System.out.println("Willkommen beim Taschenrechner!");
 
         while (true) {
             System.out.print("Geben Sie die gewünschte Operation ein (+, -, *, /): ");
@@ -28,6 +28,8 @@ public class Main {
 
             System.out.print("Geben Sie den zweiten Operanden ein: ");
             int lastOperand = scanner.nextInt();
+
+            scanner.nextLine(); // Чтобы перейти на новую строку и избежать проблем с последующим вводом.
 
             switch (operator) {
                 case "+":
@@ -43,25 +45,83 @@ public class Main {
                     break;
 
                 case "/":
-                    System.out.println(firstOperand / lastOperand);
-                    break
+                    if (lastOperand != 0) { 
+                        System.out.println(firstOperand / lastOperand);
+                    } else {
+                        System.out.println("Division by zero error.");
+                    }
+                    break;
 
                 default:
-                    System.out.println("Wie bitte?");
-
+                    System.out.println("Ungültige Operation!");
             }
 
             System.out.print("Möchten Sie eine weitere Berechnung durchführen? (ja/nein): ");
-            if (scanner.nextLine() == "nein") {
-                break
+            String continueCalculation = scanner.nextLine();
+            if (continueCalculation.equalsIgnoreCase("nein")) { 
+                System.out.println("Auf Wiedersehen!");
+                break;
             }
         }
     }
+    
 
+    public static void task3(Scanner scanner) {
+        double usdToEurRate = 0.85;
+        double eurToJpyRate = 130.0;
+        double jpyToUsdRate = 0.009;
+
+        String[] currencies = { "USD (US-Dollar)", "EUR (Euro)", "JPY (Japanischer Yen)" };
+
+        System.out.println("Willkommen beim Währungsumrechner!");
+
+        while (true) {
+            System.out.println("Verfügbare Währungen:");
+            for (int i = 0; i < currencies.length; i++) {
+                System.out.println((i + 1) + ". " + currencies[i]);
+            }
+
+            System.out.print("Geben Sie den Betrag in der Ausgangswährung ein: ");
+            double amount = scanner.nextDouble();
+
+            System.out.print("Geben Sie die Ausgangswährung (1/2/3): ");
+            int sourceCurrency = scanner.nextInt();
+
+            System.out.print("Geben Sie die Zielswährung (1/2/3): ");
+            int targetCurrency = scanner.nextInt();
+
+            double convertedAmount = 0.0;
+
+            if (sourceCurrency == 1 && targetCurrency == 2) {
+                convertedAmount = amount * usdToEurRate;
+            } else if (sourceCurrency == 2 && targetCurrency == 3) {
+                convertedAmount = amount * eurToJpyRate;
+            } else if (sourceCurrency == 3 && targetCurrency == 1) {
+                convertedAmount = amount * jpyToUsdRate;
+            } else {
+                System.out.println("Ungültige Auswahl von Währungen.");
+            }
+
+                System.out.println("Der umgerechnete Betrag ist: " + convertedAmount + " " + currencies[targetCurrency - 1]);
+
+                System.out.print("Möchten Sie eine weitere Umrechnung durchführen? (ja/nein): ");
+                String choice = scanner.next();
+
+                if (choice.equalsIgnoreCase("nein")) {
+                    System.out.println("Auf Wiedersehen!");
+                    break;
+                }
+            }
+
+    }
+
+    
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println(task1(scanner))
-        task2(scanner)
+        System.out.println(task1(scanner));
+        task2(scanner);
+        task3(scanner);
+        scanner.close(); 
     }
 }
